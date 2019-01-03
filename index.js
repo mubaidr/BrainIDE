ac06729f5a28388cdcd13e5bdc6cc45325b285ef
Misha Marinenko <a0x77@icloud.com>
2019-01-03 13:57:07 +0200

    MenuBar
    Working on menubar

--- index.js
+++ index.js
@@ -3,28 +3,9 @@
   * Copyright 2019 (c) Misha Marinenko
   * ./index.js
 */
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow } = require('electron');
 let win;
-/*
-// TODO Our own menu
-const menu = Menu.buildFromTemplate([
-    {
-        label: 'Edit',
-        submenu: [
-            { role: 'undo' },
-            { role: 'redo' },
-            { type: 'separator' },
-            { role: 'cut' },
-            { role: 'copy' },
-            { role: 'paste' },
-            { role: 'pasteandmatchstyle' },
-            { role: 'delete' },
-            { role: 'selectall' }
-        ]
-    }
-])
-Menu.setApplicationMenu(menu)
-*/
+
 function createWindow() {
     win = new BrowserWindow({ width: 1024, height: 600, title: "BrainIDE" });
     win.loadFile('app/index.html');
@@ -33,13 +14,44 @@ function createWindow() {
         win = null;
     });
 }
+
 app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
+const { Menu } = require('electron')
 
+const template = [
+    {
+        label: 'IDE',
+        submenu: [
+            { id: "1", label: 'Start' },
+            { id: "2", label: 'Stop' }
+        ]
+    }
+]
+
+if (process.platform === 'darwin') {
+    template.unshift({
+        label: app.getName(),
+        submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideothers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+        ]
+    })
+}
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 app.on('activate', () => {
     if (win === null) {
         createWindow();
